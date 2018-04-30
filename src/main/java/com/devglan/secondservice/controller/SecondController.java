09af459cf712c0e76a7d22eb2cd900fc91c90ccb
vikrantch-hk <chudhry.vikrant@Brightlife.com>
2018-04-30 15:35:19 +0530

    1

---
+++ src/main/java/com/devglan/secondservice/controller/SecondController.java
@@ -0,0 +1,13 @@
+package com.devglan.secondservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecondController {
+
+    @RequestMapping("/")
+    public String demo(){
+        return "Second service";
+    }
+}
